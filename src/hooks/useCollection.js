80b74f1b5593ae4579cc7d940407b30e2b0d70ff
Tanmay Maheshwari <tm19534cse2019@gmail.com>
2022-06-29 01:31:30 +0530

    adds error handling in onSnapshot method

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -11,14 +11,21 @@ export const useCollecton = (collectionName, _query) => {
         if (customQuery) {
             ref = query(ref, where(...customQuery));
         }
-        const unsubscribe = onSnapshot(ref, (snapshot) => {
-            let results = [];
-            snapshot.docs.forEach((doc) => {
-                results.push({ ...doc.data(), id: doc.id });
-            });
-            setDocuments(results);
-            setEmpty(snapshot.empty);
-        });
+        const unsubscribe = onSnapshot(
+            ref,
+            (snapshot) => {
+                let results = [];
+                snapshot.docs.forEach((doc) => {
+                    results.push({ ...doc.data(), id: doc.id });
+                });
+                setDocuments(results);
+                setEmpty(snapshot.empty);
+            },
+            (error) => {
+                setEmpty(true);
+                alert(error.message);
+            }
+        );
 
         return () => unsubscribe();
     }, [collectionName, customQuery]);
